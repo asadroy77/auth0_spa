9825eb8fb955e78dc0408c534ba4c1c2e5223a9f
asadroy77 <asadhanif3865@gmail.com>
2023-06-27 16:50:13 +0500

    footer changed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Box from '@mui/material/Box';
 
 
-function Footer() {
+function Footer() { 
+  //footer responsive
   return (<>
   <Box sx={{ position:"absolute" , backgroundColor:"black", left:0, right:0,bottom:0,height:"80px"}}> </Box>
   </>
